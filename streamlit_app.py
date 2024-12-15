6832221a09a22d350d95cc1228a987c46b0be46c
chyntiaadinda <chyntia.ramadani@student.president.ac.id>
2024-12-15 03:06:58 +0000

    c

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,7 +56,7 @@ def custom_heading(text, level):
     elif level == 2:
         return f"<h2 style='text-align: center; font-size: 30px; font-family: Arial, sans-serif; margin: 0 auto;'>{text}</h2>"
     elif level == 3:
-        return f"<h3 style='text-align: center; font-size: 24px; font-family: Arial, sans-serif; margin: -50;'>{text}</h3>"
+        return f"<h3 style='text-align: center; font-size: 24px; font-family: Arial, sans-serif; margin: -1000;'>{text}</h3>"
 
 # Navigasi sidebar
 with st.sidebar:
